508a9f0a0640b6c212fcfd0115f7f498d4c4e260
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-10 21:13:18 +0200

    handle lifecycle & clean up event listener

--- components/ExpandingList/ExpandingList.js
+++ components/ExpandingList/ExpandingList.js
@@ -2,26 +2,35 @@ class ExpandingList extends HTMLUListElement {
   constructor() {
     super();
 
-    const lis = this.querySelectorAll('li');
-
-    lis.forEach((li) => {
-      if (li.querySelector('ul')) {
-        li.classList.add('expandable', 'closed');
-      }
-    });
-
-    this.addEventListener('click', (e) => {
-      if (e.target.classList.contains('expandable')) {
-        e.target.classList.toggle('closed');
-      }
-    });
-
     const linkElem = document.createElement('link');
     linkElem.setAttribute('rel', 'stylesheet');
     linkElem.setAttribute('href', 'components/ExpandingList/ExpandingList.css');
 
     this.appendChild(linkElem);
     this.classList.add('expanding-list');
+  }
+
+  connectedCallback() {
+    const lis = this.querySelectorAll('li');
+
+    lis.forEach((li) => {
+      li.classList.remove('expandable', 'closed');
+      if (li.querySelector('ul')) {
+        li.classList.add('expandable', 'closed');
+      }
+    });
+
+    this.addEventListener('click', this.handleClick);
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('click', this.handleClick);
+  }
+
+  handleClick(event) {
+    if (event.target.classList.contains('expandable')) {
+      event.target.classList.toggle('closed');
+    }
   }
 }
 
